5b109a04250426242a6354a04bf08d8c2839a741
Roshan Rai <roshan.rai@krayo.io>
2024-03-18 16:56:16 +0530

    test function changes

--- test.py
+++ test.py
@@ -4,11 +4,17 @@ import time
 import pandas as pd
 import logging
 
-try:
-    with Search() as bot:
-        bot.land_first_page()
-        bot.product_search('Samsung Galaxy S10')
-        bot.apply_filters(category="Mobiles", brand="SAMSUNG", flipkart_assured="Plus (FAssured)", price="Price -- High to Low")
-        bot.get_parameters()
-except Exception as e:
-    pass
+
+def main():
+    try:
+        with Search() as bot:
+            bot.land_first_page()
+            bot.product_search('Samsung Galaxy S10')
+            bot.apply_filters(category="Mobiles", brand="SAMSUNG", flipkart_assured="Plus (FAssured)", price="Price -- High to Low")
+            bot.get_parameters()
+    except Exception as e:
+        pass
+    
+    
+if __name__ == "__main__":
+    main()
